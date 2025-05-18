e3923491e70ac2c1638c5513794bcde71e43b0e9
Shumail_Engineer <shumailsaman709@gmail.com>
2025-05-19 02:54:13 +0500

    Add files via upload
    To getrid of tierdness play a really amazing game named madlib. This game is an uncompelet story that is completed by adding word.

---
+++ Madlib_Game.py
@@ -0,0 +1,12 @@
+#Madlib Game:
+
+adjective1 = input("enter any adjective: ")
+noun = input("enter any noun: ")
+adjective2 = input("enter any 2nd adjective: ")
+verb = input("enter any verb: ")
+adjective3 = input("enter any adjective: ")
+
+print(f"Tomorow,I went to {adjective1} jungle.")
+print(f"Here i saw {noun} in the cage.")
+print(f"{noun} was very {adjective2}. he was {verb}.")
+print(f"In that trip i was very {adjective3} after seen that {noun}")
